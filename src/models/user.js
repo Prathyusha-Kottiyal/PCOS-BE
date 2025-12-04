c6ae06fd873b62358e303fa5fabfffbe6886b440
Prathyusha K <prathyushak@Prathyushas-MacBook-Pro.local>
2025-12-04 12:00:28 +0530

    body measuments added

--- src/models/user.js
+++ src/models/user.js
@@ -51,8 +51,21 @@ const userSchema = new mongoose.Schema(
         }
       },
     },
+
+    // ---- Existing fields ----
     height: { type: Number, min: 100, max: 250 },
     weight: { type: Number, min: 30, max: 200 },
+
+    // ---- NEW optional body measurements ----
+    measurements: {
+      waist: { type: Number, min: 40, max: 200 },
+      hip: { type: Number, min: 40, max: 200 },
+      chest: { type: Number, min: 40, max: 200 },
+      arm: { type: Number, min: 10, max: 80 },
+      thigh: { type: Number, min: 20, max: 120 },
+      neck: { type: Number, min: 20, max: 70 },
+    },
+
     resetPlan: {
       startDate: { type: Date },
     },
@@ -60,17 +73,20 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// JWT
 userSchema.methods.getJWT = async function () {
   const token = await jwt.sign({ _id: this._id }, "DEV567", {
     expiresIn: "7d",
   });
   return token;
 };
+
 userSchema.index({ name: 1 });
 
+// Password validation
 userSchema.methods.validatePassword = async function (passwordByUser) {
   const passwordHash = this.password;
-  const isPasswordMatch = await bcrypt.compare(passwordByUser, passwordHash);
-  return isPasswordMatch;
+  return await bcrypt.compare(passwordByUser, passwordHash);
 };
+
 module.exports = mongoose.model("User", userSchema);
